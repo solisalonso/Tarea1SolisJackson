d158994d39c208ad12b3b0c49c646442553682fa
solisalonso <solisalonso98@gmail.com>
2021-03-04 19:45:52 -0600

    update

--- pruebas.py
+++ pruebas.py
@@ -4,11 +4,18 @@ from codigo import *
 
 
 
-@pytest.mark.parametrize('ch',[('a'),('b')])
-def test(ch):
+@pytest.mark.parametrize('ch',[('a'),('b'),('c'),('d'),('e'),('f'),('g'),('h'),('i'),('j')])
+def test1(ch):
     assert check_char(ch)=='0'
 
-@pytest.mark.parametrize('ch',[('a'),('b')])
-def test(ch):
-    assert caps_switch(ch)==ch.swapcase()
+@pytest.mark.parametrize('ch',[('a')])
+def test2(ch):
+    assert caps_switch(ch)=='A'
 
+@pytest.mark.parametrize('ch',[('55')])
+def test3(ch):
+    assert check_char(ch)=='0'
+
+@pytest.mark.parametrize('ch',[('hh')])
+def test4(ch):
+    assert check_char(ch)=='0'
