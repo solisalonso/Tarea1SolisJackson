292b8fadb26f068fee998aaa7adc1a1c627c6eac
solisalonso <solisalonso98@gmail.com>
2021-03-05 10:11:27 -0600

    pruebas version final

--- pruebas.py
+++ pruebas.py
@@ -2,9 +2,7 @@ import pytest
 from codigo import *
 
 
-
-
-@pytest.mark.parametrize('ch',[('a'),('b'),('c'),('d'),('e'),('f'),('g'),('h'),('i'),('j')])
+@pytest.mark.parametrize('ch',[('a'),('b'),('c'),('d'),('e'),('f'),('g'),('h'),('i'),('j'),('k'),('l'),('m'),('n'),('o'),('p'),('q'),('r'),('s'),('t'),('u'),('v'),('w'),('x'),('y'),('z')])
 def test1(ch):
     assert check_char(ch)=='0'
 
@@ -12,10 +10,13 @@ def test1(ch):
 def test2(ch):
     assert caps_switch(ch)=='A'
 
-@pytest.mark.parametrize('ch',[('55')])
+@pytest.mark.parametrize('ch',[('hh')])
 def test3(ch):
     assert check_char(ch)=='0'
 
-@pytest.mark.parametrize('ch',[('hh')])
+@pytest.mark.parametrize('ch',[('55')])
 def test4(ch):
+    assert check_char(ch)=='0'
+@pytest.mark.parametrize('ch',[('/')])
+def test5(ch):
     assert check_char(ch)=='0'
